cb8a89c503bc4759cf69ec04a808ba88fdf176fc
bakhtiyaroveech <bakhtiyaroveech@gmail.com>
2024-07-27 18:26:13 +0500

    update routing

--- src/App/bootstrap.php
+++ src/App/bootstrap.php
@@ -2,18 +2,13 @@
 
 declare(strict_types=1);
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 require __DIR__ . "/../../vendor/autoload.php";
 
 use Framework\App;
+use App\Controllers\HomeController;
 
 $app = new App();
 
-$app->get('/');
-
-dd($app);
+$app->get('/', [HomeController::class, 'home']);
 
 return $app;
