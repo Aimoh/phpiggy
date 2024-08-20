39a0dac28ca539ad75d3237d39947b32c766d5ee
bakhtiyaroveech <bakhtiyaroveech@gmail.com>
2024-08-20 18:49:21 +0500

    Validation

--- src/App/Middleware/SessionMiddleware.php
+++ src/App/Middleware/SessionMiddleware.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Middleware;
 
+use App\Exceptions\SessionException;
 use Framework\Contracts\MiddlewareInterface;
 
 class SessionMiddleware implements MiddlewareInterface
@@ -11,8 +12,18 @@ class SessionMiddleware implements MiddlewareInterface
 
     public function process(callable $next)
     {
+        if (session_status() === PHP_SESSION_ACTIVE) {
+            throw new SessionException("Session already active.");
+        }
+
+        if (headers_sent($filename, $line)) {
+            throw new SessionException("Headers already sent. Consider enabling output buffering. Data outputted from {$filename} - Line: {$line}");
+        }
+
         session_start();
 
         $next();
+
+        session_write_close();
     }
 }
