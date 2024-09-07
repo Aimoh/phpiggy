fceb0a36eb3ad745d0da3fde6b6b0e469aeec77f
bakhtiyaroveech <bakhtiyaroveech@gmail.com>
2024-09-07 15:02:02 +0500

    env

--- cli.php
+++ cli.php
@@ -4,36 +4,11 @@ include __DIR__ . "/src/Framework/Database.php";
 
 use Framework\Database;
 
-$db = new Database('mysql', [
-    'host' => 'localhost',
-    'port' => 3306,
-    'dbname' => 'phpiggy'
-], 'root', '');
-
-//try {
-//    $db->connection->beginTransaction();
-//
-//    $db->connection->query("INSERT INTO products VALUES(99, 'GLoves')");
-//
-//    $search = "Hats";
-//    $query = "SELECT * FROM products WHERE name=:name";
-//
-//    $stmt = $db->connection->prepare($query);
-//
-//    $stmt->bindValue('name', 'Gloves', PDO::PARAM_STR);
-//
-//    $stmt->execute();
-//
-//    var_dump($stmt->fetchAll(PDO::FETCH_OBJ));
-//
-//    $db->connection->commit();
-//} catch (Exception $error) {
-//    if ($db->connection->inTransaction()) {
-//        $db->connection->rollBack();
-//    }
-//
-//    echo "Transaction failed!";
-//}
+$db = new Database($_ENV['DB_DRIVER'], [
+    'host' => $_ENV['DB_HOST'],
+    'port' => $_ENV['DB_PORT'],
+    'dbname' => $_ENV['DB_NAME'],
+], $_ENV['DB_USER'], $_ENV['DB_PASS']);
 
 $sqlFile = file_get_contents("./database.sql");
 
--- src/App/container-definitions.php
+++ src/App/container-definitions.php
@@ -3,10 +3,15 @@
 declare(strict_types=1);
 
 use App\Services\ValidatorService;
-use Framework\TemplateEngine;
+use Framework\{Database, TemplateEngine};
 use App\Config\Paths;
 
 return [
     TemplateEngine::class => fn() => new TemplateEngine(Paths::VIEW),
-    ValidatorService::class => fn() => new ValidatorService()
+    ValidatorService::class => fn() => new ValidatorService(),
+    Database::class => fn() => new Database($_ENV['DB_DRIVER'], [
+        'host' => $_ENV['DB_HOST'],
+        'port' => $_ENV['DB_PORT'],
+        'dbname' => $_ENV['DB_NAME'],
+    ], $_ENV['DB_USER'], $_ENV['DB_PASS'])
 ];
