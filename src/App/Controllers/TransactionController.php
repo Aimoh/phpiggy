8e9fec07c5ab99948722e229b3f6aeb3104ba1cb
bakhtiyaroveech <bakhtiyaroveech@gmail.com>
2024-09-25 13:10:13 +0500

    CRUD transactions

--- src/App/Controllers/TransactionController.php
+++ src/App/Controllers/TransactionController.php
@@ -10,8 +10,8 @@ use Framework\TemplateEngine;
 class TransactionController
 {
     public function __construct(
-        private TemplateEngine $view,
-        private ValidatorService $validatorService,
+        private TemplateEngine     $view,
+        private ValidatorService   $validatorService,
         private TransactionService $transactionService
     )
     {
@@ -27,6 +27,41 @@ class TransactionController
         $this->validatorService->validateTransaction($_POST);
 
         $this->transactionService->create($_POST);
+
+        redirectTo('/');
+    }
+
+    public function editView(array $params)
+    {
+        $transaction = $this->transactionService->getUserTransaction($params['transaction']);
+
+        if (!$transaction) {
+            redirectTo('/');
+        }
+
+        echo $this->view->render("transactions/edit.php", [
+            'transaction' => $transaction
+        ]);
+    }
+
+    public function edit(array $params)
+    {
+        $transaction = $this->transactionService->getUserTransaction($params['transaction']);
+
+        if (!$transaction) {
+            redirectTo('/');
+        }
+
+        $this->validatorService->validateTransaction($_POST);
+
+        $this->transactionService->update($_POST, $transaction['id']);
+
+        redirectTo($_SERVER['HTTP_REFERER']);
+    }
+
+    public function delete(array $params)
+    {
+        $this->transactionService->delete((int) $params['transaction']);
 
         redirectTo('/');
     }
